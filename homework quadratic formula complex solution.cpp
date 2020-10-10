93b6ab20718d04c80b001cde6513cf9c6ea87889
Filip Borecki <filibor847@student.polsl.pl>
2020-10-10 15:24:28 +0200

    first iteration of quadratic formula solver

---
+++ homework quadratic formula complex solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+int main(){
+
+    std::cout <<"Welcome to the quadratic formula solver!"<<std::endl;
+
+    float a; // variable for storing coefficient a
+    float b; // variable for storing coefficient b
+    float c; // variable for storing coefficient c
+
+    std::cout <<"Please input coefficient a\n"; // asking user to input coefficient a by using cout
+    std::cin >>a; // read input with cin and store it in variable a
+
+    std::cout <<"Please input coefficient b\n"; // asking user to input coefficient b by using cout
+    std::cin >>b; // read input with cin and store it in variable b
+
+    std::cout <<"Please input coefficient c\n"; // asking user to input coefficient c by using cout
+    std::cin >>c; // read input with cin and store it in variable c
+
+    if(a == 0 && b == 0){  // first pattern of solving quadratic formula
+        float x = 0; //variable to store x value
+        std::cout<< "Your x is: " << x <<std::endl; // printing x value
+    }
+    else if ( a == 0)     // second pattern of solving quadratic formula
+    {
+        float x = -c/b;  //variable to store x value
+        std::cout<< "Your x is: " << x <<std::endl; // printing x value
+    }
+    else if ( b == 0 && ((a < 0 && c > 0) || ( a > 0 && c < 0))) // third pattern of solving quadratic formula
+    {
+       float x = sqrtf (-c/a); //variable to store x value
+        std::cout<< "Your x is: " << x <<std::endl; // printing x value
+
+    }
+    else if ( b == 0 && ((a > 0 && c > 0) || ( a < 0 && c < 0))) // continuation of the third pattern
+    {
+        std::cout<< "Your x can't be defined " <<std::endl; // printing x value
+
+    }
+    else if ( c == 0 ) // fourth pattern of solving quadratic formula
+    {
+        float x1 = 0; // variable to store x1 value
+        float x2 = -b/a; // variable to store x2 value
+        std::cout<<"Your x values are: "<< x1; std::cout <<" and "<< x2 <<std::endl; // printing values of both x1 and x2
+    }
+
+}

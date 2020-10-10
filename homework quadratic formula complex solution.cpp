234458e3483bd5b7c0a850c2a6b39bcf15baad17
Filip Borecki <filibor847@student.polsl.pl>
2020-10-10 20:39:38 +0200

    complete quadratic formula solver

--- homework quadratic formula complex solution.cpp
+++ homework quadratic formula complex solution.cpp
@@ -17,18 +17,18 @@ int main(){
     std::cout <<"Please input coefficient c\n"; // asking user to input coefficient c by using cout
     std::cin >>c; // read input with cin and store it in variable c
 
-    if(a == 0 && b == 0){  // first pattern of solving quadratic formula
-        float x = 0; //variable to store x value
-        std::cout<< "Your x is: " << x <<std::endl; // printing x value
+    if(a == 0 && b == 0) // first pattern of solving quadratic formula
+    {
+        std::cout<< "Your x can't be defined " <<std::endl; // printing x value
     }
     else if ( a == 0)     // second pattern of solving quadratic formula
     {
-        float x = -c/b;  //variable to store x value
+        float x = -c/b;  //variable to compute and store x value
         std::cout<< "Your x is: " << x <<std::endl; // printing x value
     }
     else if ( b == 0 && ((a < 0 && c > 0) || ( a > 0 && c < 0))) // third pattern of solving quadratic formula
     {
-       float x = sqrtf (-c/a); //variable to store x value
+       float x = sqrtf (-c/a); //variable to compute and store x value
         std::cout<< "Your x is: " << x <<std::endl; // printing x value
 
     }
@@ -37,11 +37,35 @@ int main(){
         std::cout<< "Your x can't be defined " <<std::endl; // printing x value
 
     }
-    else if ( c == 0 ) // fourth pattern of solving quadratic formula
+    else if ( c == 0 && (b == 0 || a == 0) ) // fourth pattern of solving quadratic formula
+    {
+        float x = 0; //variable to store x value
+         std::cout<<"Your x is: "<< x <<std::endl;    // printing x value
+    }
+    else if ( c == 0) // continuation of fourth pattern
     {
         float x1 = 0; // variable to store x1 value
-        float x2 = -b/a; // variable to store x2 value
+        float x2 = -b/a; // variable to compute and store x2 value
         std::cout<<"Your x values are: "<< x1; std::cout <<" and "<< x2 <<std::endl; // printing values of both x1 and x2
     }
-
+    else if (a != 0 && b != 0 && c != 0) // fifth pattern of solving quadratic formula
+    {
+        float discriminant = powf(b,2)-4*a*c; // variable to store and compute discriminant value
+      if(discriminant > 0) // course of action regarding the value of discriminant
+      {
+            float x1 = (-b+discriminant)/(2*a); // variable to compute and store x1 value
+            float x2 = (-b-discriminant)/(2*a); // variable to compute and store x2 value
+            std::cout<<"Your x values are: "<< x1; std::cout <<" and "<< x2 <<std::endl; // printing both x1 and x2 values
+        }
+      else if(discriminant == 0) // course of action regarding the value of discriminant
+      {
+          float x0 = -b/(2*a); // variable to store and compute x0 value
+          std::cout<<"Your x is: "<< x0 <<std::endl; // printing x0 value
+      }
+      else if(discriminant < 0) // course of action regarding the value of discriminant
+      {
+          std::cout<<"Your x can't be defined"<<std::endl; // printing x value
+      }
+    }
+    return 0;
 }

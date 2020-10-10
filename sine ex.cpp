9ebb7ad443bdb44a9cf89ef4461ab116324de234
Filip Borecki <filibor847@studen.polsl.pl>
2020-10-10 13:02:35 +0200

    simple sine ex

---
+++ sine ex.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    for ( int i = 0; i <= 360; i++)
+    {
+        std::cout<<"Angle: "<< i << " / sine = " << sin(i) <<std::endl;
+    }
+    return 0;
+}
